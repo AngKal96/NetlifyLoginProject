8cd50e3ff1553049bbb82f0fb6744c2ba9be2510
Angela Kalogeras <akalogeras@proforma.com>
2022-07-13 14:05:33 -0400

    changes

--- myScript.js
+++ myScript.js
@@ -1,24 +1,95 @@
-netlifyIdentity.open();
+// let app = angular.module("cleanBlog", ["ngRoute"]);
 
-// Get the current user:
-// Available after on('init') is invoked
-const user = netlifyIdentity.currentUser();
+// app.config(function ($routeProvider) {
+//     $routeProvider
+//         .when("/", {
+//             templateUrl: "login.html",
+//         })
+//         .when("/login", {
+//             templateUrl: "login.html",
+//         })
+//         .when("/blog", {
+//             templateUrl: "blog.html",
+//         })
+// });
 
-// Bind to events
-netlifyIdentity.on('init', user => console.log('init', user));
-netlifyIdentity.on('login', user => console.log('login', user));
-netlifyIdentity.on('logout', () => console.log('Logged out'));
-netlifyIdentity.on('error', err => console.error('Error', err));
-netlifyIdentity.on('open', () => console.log('Widget opened'));
-netlifyIdentity.on('close', () => console.log('Widget closed'));
+//let user = netlifyIdentity.currentUser();
 
-// Unbind from events
-netlifyIdentity.off('login'); // to unbind all registered handlers
-netlifyIdentity.off('login', handler); // to unbind a single handler
+netlifyIdentity.on('init', () => {
+    initUser = netlifyIdentity.currentUser();
+    //console.log(user);
+});
 
-// Close the modal
-netlifyIdentity.close();
 
-// Log out the user
-netlifyIdentity.logout();
+
+netlifyIdentity.on('login', () => {
+    if (initUser !== null) {
+        window.location.replace('blog');
+    }
+    netlifyIdentity.close();
+});
+
+netlifyIdentity.on('logout', () => {
+    netlifyIdentity.close();
+    window.location.replace('index');
+});
+
+
+// netlifyIdentity.open = () => {
+
+//     exports.handler = async (event, context) => {
+//         const isLoggedIn = document.querySelector(".isLoggedIn");
+//         const notLoggedIn = document.querySelector(".notLoggedIn");
+
+//         if (context.clientContext.user) {
+//             return {
+//                 statusCode: 200,
+//                 body: JSON.stringify(isLoggedIn)
+//             };
+//         }
+
+//         return {
+//             statusCode: 401,
+//             body: JSON.stringify(notLoggedIn)
+//         }
+//     };
+
+
+    // netlifyIdentity.on('init', user => {
+
+    // });
+
+
+
+    // // Get the current user:
+    // // Available after on('init') is invoked
+
+
+    // // Bind to events
+
+// };
+// netlifyIdentity.on('login', user => console.log('login', user));
+// netlifyIdentity.on('logout', () => console.log('Logged out'));
+// netlifyIdentity.on('error', err => console.error('Error', err));
+// netlifyIdentity.on('open', () => console.log('Widget opened'));
+// netlifyIdentity.on('close', () => console.log('Widget closed'));
+
+// // // Unbind from events
+// netlifyIdentity.off('login'); // to unbind all registered handlers
+// netlifyIdentity.off('login', handler); // to unbind a single handler
+
+// // // Close the modal
+// netlifyIdentity.close();
+
+// // // Log out the user
+// netlifyIdentity.logout();
+
+
+    // isLoggedIn = () => {
+
+    //     netlifyIdentity.on('login', user => {
+    //         userLoggedOn === true;
+    //     }
+    //     )
+    // };
 
