66a454ecd29581f86c72732efabe74581fb584f4
Angela Kalogeras <akalogeras@proforma.com>
2022-07-13 09:29:57 -0400

    netlify project

---
+++ Netlify Login Project/myScript.js
@@ -0,0 +1,23 @@
+netlifyIdentity.open();
+
+// Get the current user:
+// Available after on('init') is invoked
+const user = netlifyIdentity.currentUser();
+
+// Bind to events
+netlifyIdentity.on('init', user => console.log('init', user));
+netlifyIdentity.on('login', user => console.log('login', user));
+netlifyIdentity.on('logout', () => console.log('Logged out'));
+netlifyIdentity.on('error', err => console.error('Error', err));
+netlifyIdentity.on('open', () => console.log('Widget opened'));
+netlifyIdentity.on('close', () => console.log('Widget closed'));
+
+// Unbind from events
+netlifyIdentity.off('login'); // to unbind all registered handlers
+netlifyIdentity.off('login', handler); // to unbind a single handler
+
+// Close the modal
+netlifyIdentity.close();
+
+// Log out the user
+netlifyIdentity.logout();
